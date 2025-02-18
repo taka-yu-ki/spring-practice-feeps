c8650762fb33e1fbbf4dd34cf9513c5f600b906f
taka-yu-ki <taka7_zegs@icloud.com>
2025-02-18 16:20:11 +0900

    initial commit

---
+++ src/main/java/jp/co/spring_practice_feeps/SpringPracticeFeepsApplication.java
@@ -0,0 +1,13 @@
+package jp.co.spring_practice_feeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeFeepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeFeepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/spring_practice_feeps/SpringPracticeFeepsApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.spring_practice_feeps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeFeepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
